e1556bfaf52ba8690df5bb2d3f732bc4ff958c02
Connor Runyan <r.and.d.guy@gmail.com>
2025-05-10 10:02:20 -0400

    criminal

--- player_bot.gd
+++ player_bot.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const SPEED = 500.0
-
+const turn_speed = 1.0
 @onready var left_tread_anim = $LeftTread/LeftTreadAnimationPlayer
 @onready var right_tread_anim = $RightTread/RightTreadAnimationPlayer
 
@@ -16,14 +16,18 @@ func _process(delta):
 	var mouse_pos:Vector2 = get_canvas_transform().affine_inverse() * get_viewport().get_mouse_position()
 	var heading = (position - mouse_pos).normalized()
 	var target_rotation = atan2(heading.y, heading.x)
-	rotation = lerp_angle(rotation, target_rotation, 0.1)
+	rotation = lerp_angle(rotation, target_rotation, turn_speed*delta) # TODO rework this to use a constant turn radius 
+														# TODO so we don't have it turn faster the farther ur aiming
+	
+	var target_camera_pos = mouse_pos.lerp(global_position, 0.8)
+	camera.global_position = target_camera_pos
 	
 	if Input.is_action_pressed("forward"):
-		position+=heading * -SPEED * delta
+		position+=rotation_to_direction(rotation) * -SPEED * delta
 		left_tread_anim.play("tread_forward")
 		right_tread_anim.play("tread_forward")
 	elif Input.is_action_pressed("backward"):
-		position+=heading * SPEED * delta
+		position+=rotation_to_direction(rotation) * SPEED * delta
 		left_tread_anim.play("tread_backward")
 		right_tread_anim.play("tread_backward")
 	else:
@@ -45,3 +49,15 @@ func _process(delta):
 
 func _input(event):
 	pass
+
+func rotation_to_direction(rotation_radians: float) -> Vector2:
+	# Convert rotation from degrees to radians (skip if already in radians)
+	#var rotation_radians = deg_to_rad(rotation_degrees)
+	
+	# Calculate direction vector
+	var direction = Vector2(cos(rotation_radians), sin(rotation_radians))
+	
+	# Normalize the vector (optional, but ensures length = 1)
+	direction = direction.normalized()
+	
+	return direction
