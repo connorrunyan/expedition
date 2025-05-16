76bc1663bb429fe5db5d74a66ba6c2aff7f78401
Connor Runyan <r.and.d.guy@gmail.com>
2025-05-16 15:58:30 -0400

    lul

--- stats.gd
+++ stats.gd
@@ -7,5 +7,5 @@ var player_shield_max = 100.0
 var player_heat_current = 100.0
 var player_heat_threshold = 100.0
 
-var player_turn_radius = 2.0
-var player_move_speed = 500.0
+var player_turn_radius = 90.0
+var player_move_speed = 1420.0
