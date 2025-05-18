caf58db93bd75eb5ccf0fd1b0b86d34449df88c6
Dale-Bruhns <DEUWA.BRUHNS@HOTMAIL.COM>
2025-05-19 03:17:15 +0800

    Added a bunch of upgrades

--- upgrade.gd
+++ upgrade.gd
@@ -99,56 +99,80 @@ func _on_panel_mouse_exited():
 
 var normal_upsides = [
 	upgrade_data.new("+%d\nPlayer Bullet Damage", "player_bullet_damage", 1),
-	upgrade_data.new("+%d\nPlayer Laser Damage", "player_laser_damage", 1),
 	upgrade_data.new("+%d\nPlayer Turn Radius", "player_turn_radius", 1),
 	upgrade_data.new("+%d\nPlayer Move Speed", "player_move_speed", 20),
+	upgrade_data.new("+%d\nPlayer Health", "player_hp_current", 10),
+	upgrade_data.new("+%d\nPlayer Turn Speed", "player_turn_radius", 5),
+	upgrade_data.new("+%d\nExplosion Wind Up", "explode_Timer", 0.2),
+	upgrade_data.new("+%d\nExplosionsion Detection Radius", "explode_detection_radius", +30),
 	]
 	
 var normal_downsides = [
 	upgrade_data.new("%d\nPlayer Bullet Damage", "player_bullet_damage", -1),
-	upgrade_data.new("%d\nPlayer Laser Damage", "player_laser_damage", -1),
 	upgrade_data.new("%d\nPlayer Turn Radius", "player_turn_radius", -1),
 	upgrade_data.new("%d\nPlayer Move Speed", "player_move_speed", -20),
+	upgrade_data.new("+%d\nExploder Move Speed", "explode_movement_speed", 10),
+	upgrade_data.new("+%d\nCharger Move Speed", "charge_movement_speed", 10),
+	upgrade_data.new("+%d\nSpike Strip Move Speed", "spike_movement_speed", 10),
+	upgrade_data.new("+%d\nExploder Level", "explode_Level", 1),
+	upgrade_data.new("+%d\nCharger Level", "charge_Level", 1),
+	upgrade_data.new("+%d\nSpike Strip Level", "spike_Level", 1),
+	upgrade_data.new("+%d\nShooter Level", "shooty_Level", 1),
+	upgrade_data.new("%d\nPlayer Move Speed", "player_move_speed", -100),
+	upgrade_data.new("+%d\nExploder Damage", "explode_Damage", 1),
+	upgrade_data.new("+%d\nCharger WindUp Time", "charge_WindUpTime", 1),
+	upgrade_data.new("+%d\nCharger Charge Velocity", "charge_chargeVelocity", 100),
+	upgrade_data.new("+%d\nCharger Charge Distance", "charge_ChargeDistance", 100),
+	upgrade_data.new("+%d\nSpike Strip Charge Distance", "spike_ChargeDistance", 100),
+	upgrade_data.new("+%d\nCharger Charge Damage", "charge_Damage", 2),
+	upgrade_data.new("+%d\nSpike Strip WindUp Time", "spike_WindUpTime", 1),
+	upgrade_data.new("+%d\nSpike Strip Charge Velocity", "spike_chargeVelocity", 100),
+	upgrade_data.new("+%d\nShooty movement speed", "shooty_movement_speed", 100),
+	upgrade_data.new("+%d\nShooty Damage", "shooty_Damage", 1),
+	upgrade_data.new("+%d\nShooty Projectile Speed", "shooty_projectile_speed", 100),
 	]
 	
 var rare_upsides = [
 	upgrade_data.new("+%d\nPlayer Bullet Damage", "player_bullet_damage", 3),
-	upgrade_data.new("+%d\nPlayer Laser Damage", "player_laser_damage", 3),
 	upgrade_data.new("+%d\nPlayer Turn Radius", "player_turn_radius", 3),
 	upgrade_data.new("+%d\nPlayer Move Speed", "player_move_speed", 50),
+	upgrade_data.new("%d\nExploder Damage", "explode_Damage", -1),
+	upgrade_data.new("+%d\nExplosion Wind Up", "explode_Timer", 0.5),
+	upgrade_data.new("%d\nCharger Charge Velocity", "charge_chargeVelocity", -300),
+	upgrade_data.new("%d\nExploder Damage", "explode_Damage", -1),
+	upgrade_data.new("%d\nCharger Charge Damage", "charge_Damage", -1),
 	]
 	
 var rare_downsides = [
 	upgrade_data.new("%d\nPlayer Bullet Damage", "player_bullet_damage", -2),
-	upgrade_data.new("%d\nPlayer Laser Damage", "player_laser_damage", -2),
 	upgrade_data.new("%d\nPlayer Turn Radius", "player_turn_radius", -2),
 	upgrade_data.new("%d\nPlayer Move Speed", "player_move_speed", -30),
+	upgrade_data.new("+%d\nExploder Level", "explode_Level", 3),
+	upgrade_data.new("+%d\nCharger Level", "charge_Level", 3),
+	upgrade_data.new("+%d\nSpike Strip Level", "spike_Level", 3),
+	upgrade_data.new("+%d\nShooter Level", "shooty_Level", 3),
 	]
 	
 var legendary_upsides = [
 	upgrade_data.new("+%d\nPlayer Bullet Damage", "player_bullet_damage", 10),
-	upgrade_data.new("+%d\nPlayer Laser Damage", "player_laser_damage", 10),
 	upgrade_data.new("+%d\nPlayer Turn Radius", "player_turn_radius", 10),
 	upgrade_data.new("+%d\nPlayer Move Speed", "player_move_speed", 150),
 	]
 	
 var legendary_downsides = [
 	upgrade_data.new("%d\nPlayer Bullet Damage", "player_bullet_damage", -10),
-	upgrade_data.new("%d\nPlayer Laser Damage", "player_laser_damage", -10),
 	upgrade_data.new("%d\nPlayer Turn Radius", "player_turn_radius", -6),
 	upgrade_data.new("%d\nPlayer Move Speed", "player_move_speed", -70),
 	]
 	
 var exotic_upsides = [
 	upgrade_data.new("+%d\nPlayer Bullet Damage", "player_bullet_damage", 50),
-	upgrade_data.new("+%d\nPlayer Laser Damage", "player_laser_damage", 50),
 	upgrade_data.new("+%d\nPlayer Turn Radius", "player_turn_radius", 50),
 	upgrade_data.new("+%d\nPlayer Move Speed", "player_move_speed", 500),
 	]
 	
 var exotic_downsides = [
 	upgrade_data.new("%d\nPlayer Bullet Damage", "player_bullet_damage", -15),
-	upgrade_data.new("%d\nPlayer Laser Damage", "player_laser_damage", -15),
 	upgrade_data.new("%d\nPlayer Turn Radius", "player_turn_radius", -15),
 	upgrade_data.new("%d\nPlayer Move Speed", "player_move_speed", -200),
 	]
