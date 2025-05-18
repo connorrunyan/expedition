ac01426672fba05c0d6d9d339839730b24b0df7f
Dale-Bruhns <DEUWA.BRUHNS@HOTMAIL.COM>
2025-05-19 01:04:58 +0800

    Added Credits placeholder, and master volume slider.

--- title_screen.gd
+++ title_screen.gd
@@ -1,19 +1,18 @@
 extends Control
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+@onready var title = $Title
+@onready var credits = $"Credits container"
 
 func _on_end_pressed():
 	get_tree().quit()
 
-
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Enemy Test Scene.tscn")
+
+
+func _on_credits_pressed():
+	if title.visible:
+		title.visible = false
+		credits.visible = true
+	else:
+		title.visible = true
+		credits.visible = false
